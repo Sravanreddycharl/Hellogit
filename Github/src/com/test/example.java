9e9221e95eedbc7fd31280281daab4f5c87fc3a5
user1 <something@gmail.com>
2017-06-04 21:21:19 -0400

    Commit HelloGit

---
+++ Github/src/com/test/example.java
@@ -0,0 +1,9 @@
+package com.test;
+
+public class example {
+	public static void main(String[] args) {
+		String something = "a";
+		example2 e = new example2();
+		System.out.println(e.returnString(something));
+	}
+}
---
+++ Github/src/com/test/example2.java
@@ -0,0 +1,9 @@
+package com.test;
+
+public class example2 {
+
+	public String returnString(String a) {
+
+		return (a + a);
+	}
+}
